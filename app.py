1bba95fae8339149917fded4f79a663b0a2ad697
Meir Rotstein <meir@rotstein.co.il>
2023-08-27 23:20:52 +0300

    reading google sheets file and exposing via api

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+from gsheets import GSheets
+import json
 from flask import Flask, Response, send_from_directory
 app = Flask(__name__)
 
-@app.route('/api')
-def serve_api():
+@app.route('/api/sheets/<sheetId>')
+def serve_sheets_api(sheetId):
+    sheet = GSheets(sheetId)
     response = Response()
     response.headers['Content-Type'] = 'application/json'
-    response.data = '{"message": "Hello, World!"}'
+    json_data = json.dumps(sheet.read(), ensure_ascii=False).encode('utf-8')
+    response.data = json_data
     return response
 @app.route('/')
 @app.route('/dashboard/<path:file_path>')
---
+++ gsheets.py
@@ -0,0 +1,18 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class GSheets:
+    def __init__(self, sheetId):
+        self.sheetId = sheetId
+        # Define the scope and credentials
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            'rotstein.co.il_api-project-405201608861-397ee7fae789.json', scope)
+        self.gc = gspread.authorize(credentials)
+
+    def read(self):
+      sheet = self.gc.open_by_key(self.sheetId)
+      worksheet = sheet.get_worksheet(0)
+      data = worksheet.get_all_values()
+      return data
