9cead51987cd520293c7275fb33e7f600883d139
Meir Rotstein <meir@rotstein.co.il>
2023-09-03 02:25:06 +0300

    add all companies info + style  adjustments

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Dashboard from './Dashboard';
+import { Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 const darkTheme = createTheme({
   palette: {
@@ -9,10 +11,30 @@ const darkTheme = createTheme({
   },
 });
 
+const HeaderPrimary = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h5,
+  textAlign: 'center',
+  color: theme.palette.text.primary,
+  marginTop: theme.spacing(3)
+}));
+
+const HeaderSecondary = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h6,
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  marginTop: theme.spacing(1)
+}));
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <HeaderPrimary>
+        מעקב נוכחות - אימון IMI
+      </HeaderPrimary>
+      <HeaderSecondary>
+        6.9.23
+      </HeaderSecondary>
       <Dashboard/>
     </ThemeProvider>
   );
--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Paper } from '@mui/material';
+import { CircularProgress, Grid, Paper } from '@mui/material';
 import { loadData } from './Client';
 import { styled } from '@mui/material/styles';
 
@@ -14,6 +14,13 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
+  borderRadius: 0,
+  fontSize: theme.spacing(2)
+}));
+
+const TitleItem= styled(Item)(({ theme }) => ({
+  fontWeight: theme.typography.fontWeightBold,
+  fontSize: theme.spacing(3)
 }));
 
 function Dashboard() {
@@ -33,20 +40,42 @@ function Dashboard() {
     fetchData();
   }, []);
 
+  const entries = {
+    companyA: 'פלוגה א',
+    companyB: 'פלוגה ב',
+    companyC: 'פלוגה ג',
+    companyMST: 'מסייעת',
+    companyMFK: 'מפקדה',
+  }
+
   return (
     <DashboardContainer>
       {data ? (<Grid container spacing={2}>
         <Grid item xs={12}>
-          <Item>{`סה"כ הגיעו: ${data.totalArrived} מתוך ${data.totalExpected}`}</Item>
+          <TitleItem>{`סה"כ הגיעו: ${data.totalArrived} מתוך ${data.totalExpected}`}</TitleItem>
         </Grid>
         <Grid item xs={6}>
-          <Item>xs=4</Item>
+          <TitleItem>פלוגה א</TitleItem>
+          <Item>{`${data[entries.companyA].arrived} מתוך ${data[entries.companyA].expected}`}</Item>
         </Grid>
         <Grid item xs={6}>
-          <Item>xs=8</Item>
+          <TitleItem>פלוגה ב</TitleItem>
+          <Item>{`${data[entries.companyB].arrived} מתוך ${data[entries.companyB].expected}`}</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <TitleItem>פלוגה ג</TitleItem>
+          <Item>{`${data[entries.companyC].arrived} מתוך ${data[entries.companyC].expected}`}</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <TitleItem>מסייעת</TitleItem>
+          <Item>{`${data[entries.companyMST].arrived} מתוך ${data[entries.companyMST].expected}`}</Item>
+        </Grid>
+        <Grid item xs={12}>
+          <TitleItem>מפקדה</TitleItem>
+          <Item>{`${data[entries.companyMFK].arrived} מתוך ${data[entries.companyMFK].expected}`}</Item>
         </Grid>
       </Grid>) : (
-        <p>Loading...</p>
+        <CircularProgress disableShrink />
       )}
       {/* {data ? (
         <div>
