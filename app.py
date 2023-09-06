59aedaa8f93393437a8eb62236d419153ea97786
Meir Rotstein <meir@rotstein.co.il>
2023-09-06 15:44:21 +0300

    implement cache for gsheet calls

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from gsheets import GSheets
 import json
-from flask import Flask, Response, send_from_directory
+from flask import Flask, Response, send_from_directory, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:*"}})
 
 @app.route('/api/sheets/<sheetId>')
 def serve_sheets_api(sheetId):
+    cacheParam = request.args.get('cache', 'false').lower()
     sheet = GSheets(sheetId)
     response = Response()
     response.headers['Content-Type'] = 'application/json'
-    json_data = json.dumps(sheet.read(), ensure_ascii=False).encode('utf-8')
+    json_data = json.dumps(sheet.read(cacheParam == 'true'), ensure_ascii=False).encode('utf-8')
     response.data = json_data
     return response
 @app.route('/')
--- gsheets.py
+++ gsheets.py
@@ -1,18 +1,35 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+
+# global variable for caching
+lastReadCall = None
+lastReadResult = None
 
 
 class GSheets:
     def __init__(self, sheetId):
         self.sheetId = sheetId
         # Define the scope and credentials
-        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
             'rotstein.co.il_api-project-405201608861-397ee7fae789.json', scope)
-        self.gc = gspread.authorize(credentials, client_factory=gspread.client.BackoffClient)
+        self.gc = gspread.authorize(
+            credentials, client_factory=gspread.client.BackoffClient)
 
-    def read(self):
-      sheet = self.gc.open_by_key(self.sheetId)
-      worksheet = sheet.get_worksheet(0)
-      data = worksheet.get_all_values()
-      return data
+    def read(self, useCache):
+        global lastReadCall, lastReadResult
+        if (useCache and lastReadCall is not None and lastReadResult is not None):
+            now = datetime.datetime.now()
+            difference = now - lastReadCall
+            if (difference.total_seconds() <= 2):
+                print('using cache for read call')
+                return lastReadResult
+        
+        lastReadCall = datetime.datetime.now()
+        sheet = self.gc.open_by_key(self.sheetId)
+        worksheet = sheet.get_worksheet(0)
+        lastReadResult = worksheet.get_all_values()
+        print('save timestamp and data for future cache', lastReadCall)
+        return lastReadResult
