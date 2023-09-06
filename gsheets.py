6f00cdcb2a6e1a9ee532312328097e6df4a4b7d9
Meir Rotstein <meir@rotstein.co.il>
2023-09-06 07:45:33 +0300

    use BackoffClient to handle usage limit

--- gsheets.py
+++ gsheets.py
@@ -9,7 +9,7 @@ class GSheets:
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
             'rotstein.co.il_api-project-405201608861-397ee7fae789.json', scope)
-        self.gc = gspread.authorize(credentials)
+        self.gc = gspread.authorize(credentials, client_factory=gspread.client.BackoffClient)
 
     def read(self):
       sheet = self.gc.open_by_key(self.sheetId)
